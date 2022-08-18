4dd5421ddd942488148cc6309f79112a7795ee63
brownWhite <110554520+brownWhite@users.noreply.github.com>
2022-08-18 13:21:19 +0700

    Update hacking_script.py

--- NekoRobot/hacking_script.py
+++ NekoRobot/hacking_script.py
@@ -1,6 +1,6 @@
 PM_START_TEXT = """
 ────「 [{}](https://telegra.ph/file/930ec5732d9cba2e8e703.jpg) 」────
-*やあ! {} Hy, Iam Mirc Robot from Mirc_Grup,
+*やあ! {} Hy, Iam Mirc Robot from @Mirc_Grup,
 I am advance group management bot with a lot of features.*
 ➖➖➖➖➖➖➖➖➖➖➖➖➖
 ❍ *Uptime:* `{}`
